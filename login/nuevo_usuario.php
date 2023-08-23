38eea69bb043161071fda976b4ba5c09e8b406d7
Hatt3rPi <fabarca212@gmail.com>
2023-08-22 22:44:18 -0400

    Update nuevo_usuario.php

--- login/nuevo_usuario.php
+++ login/nuevo_usuario.php
@@ -1,43 +1,35 @@
 <?php
 // Incluir el archivo de conexión a la base de datos
-require_once "/home/gestio10/procedimientos_almacenados/config_ayun.php";
-
-// Verificar si se ha establecido la conexión a la base de datos
-try {
-    // Attempt to connect to MySQL database using PDO
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
-    // If connection fails, show error message and stop script
-    die("ERROR: No se pudo conectar a la base de datos. " . $e->getMessage());
-}
+require_once "/home/gestio10/procedimientos_almacenados/config_reccius.php";
 
 // Verificar si el formulario ha sido enviado
 if (isset($_POST['submit'])) {
     // Obtener el nombre de usuario y las contraseñas ingresadas y filtrarlos
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-    $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_STRING);
+    $username = mysqli_real_escape_string($link, $_POST['username']);
+    $password = mysqli_real_escape_string($link, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($link, $_POST['confirm_password']);
 
     // Verificar si el nombre de usuario ya existe en la base de datos
-    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE usuario = :usuario");
-    $stmt->execute(array('usuario' => $username));
-    $usuario = $stmt->fetch();
+    $stmt = mysqli_prepare($link, "SELECT * FROM usuarios WHERE usuario = ?");
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $usuario = mysqli_fetch_assoc($result);
 
     if ($usuario) {
         // El nombre de usuario ya existe en la base de datos, mostrar un mensaje de error
         $error = "El nombre de usuario ya está en uso.";
-    } else if ($password !== $confirm_password) {
+    } elseif ($password !== $confirm_password) {
         // Las contraseñas no coinciden, mostrar un mensaje de error
         $error = "Las contraseñas no coinciden.";
     } else {
         // Hashear la contraseña utilizando password_hash()
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        
+
         // Consulta preparada para insertar el nuevo usuario en la base de datos
-        $stmt = $pdo->prepare("INSERT INTO usuarios (usuario, contrasena) VALUES (:usuario, :contrasena)");
-        $stmt->execute(array('usuario' => $username, 'contrasena' => $hashed_password));
+        $stmt = mysqli_prepare($link, "INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)");
+        mysqli_stmt_bind_param($stmt, "ss", $username, $hashed_password);
+        mysqli_stmt_execute($stmt);
 
         // Redirigir al usuario a la página de inicio de sesión
         header("Location: login.php");
@@ -45,13 +37,14 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Nuevo usuario</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="a/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <div class="container">
