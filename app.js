080f286d4e63a3688132ce0b949eac75f3cce8b5
lucifxr63 <63132793+lucifxr63@users.noreply.github.com>
2023-11-16 18:57:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Requerir el módulo de conexión a la base de datos que has creado
+const pool = require('./src/database/database'); // Asegúrate de que la ruta es correcta
+
+// Probar la conexión a la base de datos al iniciar la aplicación
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos establecida con éxito.');
+  connection.release(); // No olvides liberar la conexión
+});
+
+// Definir una ruta GET para la raíz de tu aplicación
 app.get('/reccius', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+// Iniciar el servidor Express
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Aplicación de ejemplo escuchando en el puerto ${port}`);
+});
