74f7e32f985df1832534183b2378c18d6ea3d534
Hatt3rPi <fabarca212@gmail.com>
2023-11-16 00:03:16 -0300

    test2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/reccius/node.js', (req, res) => {
+app.get('/reccius', (req, res) => {
   res.send('Hello World!')
 })
 
