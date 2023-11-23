21d18749fe9dd619210ce0378e960fd7abe50d02
lucifxr63 <lucianoalonso2000@gmail.com>
2023-11-23 00:54:15 -0300

    node delete, front update

--- src/routers/router.test.js
+++
@@ -1,8 +0,0 @@
-// routes/dataRoutes.js
-const express = require('express');
-const router = express.Router();
-const {getData} = require('../controller/controller.test');
-
-router.get('/datos', getData);
-
-module.exports = router;
