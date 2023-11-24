61ce65117cdb73653dddabfc8062405469939b23
Hatt3rPi <fabarca212@gmail.com>
2023-11-24 16:31:49 -0300

    roles

--- pages/backend/roles/roles.php
+++ pages/backend/roles/roles.php
@@ -1,17 +1,20 @@
 <?php
 require_once "/home/customw2/conexiones/config_reccius.php";
 
+header('Content-Type: application/json');
+
+$roles = getRoles();
+echo json_encode($roles);
+
 function getRoles() {
     global $link;
     $stmt = mysqli_prepare($link, "SELECT id, nombre FROM roles");
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-
     $roles = [];
     while($row = mysqli_fetch_assoc($result)) {
         $roles[] = $row;
     }
     return $roles;
 }
-
 ?>
