8cf5152a065564d73206f113d8fe56e4ff2fa953
Hatt3rPi <fabarca212@gmail.com>
2023-11-28 00:27:30 -0300

    Update crear_usuario.php

--- pages/backend/usuario/crear_usuario.php
+++ pages/backend/usuario/crear_usuario.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once "/home/customw2/conexiones/config_reccius.php";
-// Función para validar y limpiar datos
+
 function limpiarDato($dato) {
     $dato = trim($dato);
     $dato = stripslashes($dato);
@@ -10,32 +10,42 @@ function limpiarDato($dato) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Asegúrate de que todos los campos requeridos estén configurados
-    if (isset($_POST['nombreUsuario']) && isset($_POST['correoElectronico']) && isset($_POST['usuario']) && isset($_POST['rol'])) {
+    if (isset($_POST['nombreUsuario'], $_POST['correoElectronico'], $_POST['usuario'], $_POST['rol'])) {
         
-        // Limpiar los datos para evitar inyecciones SQL
         $nombreUsuario = limpiarDato($_POST['nombreUsuario']);
         $correoElectronico = limpiarDato($_POST['correoElectronico']);
         $usuario = limpiarDato($_POST['usuario']);
         $rol = limpiarDato($_POST['rol']);
 
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-            // Establecer el modo de error de PDO a excepción
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Crear conexión
+        $link = mysqli_connect("localhost", "tu_usuario", "tu_contraseña", "nombre_de_tu_base_de_datos");
 
-            $sql = "INSERT INTO usuarios (nombreUsuario, correoElectronico, usuario, rol) VALUES (:nombreUsuario, :correoElectronico, :usuario, :rol)";
-
-            $stmt = mysqli_prepare($link, "SELECT contrasena FROM usuarios WHERE usuario = ?");
-            mysqli_stmt_bind_param($stmt, "s", $usuario);
-            mysqli_stmt_execute($stmt);
-
-            echo "Usuario creado exitosamente";
-        } catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
+        // Verificar la conexión
+        if (!$link) {
+            die("Conexión fallida: " . mysqli_connect_error());
         }
 
-        $conn = null;
+        // Verificar si el usuario ya existe
+        $stmt = mysqli_prepare($link, "SELECT id FROM usuarios WHERE usuario = ?");
+        mysqli_stmt_bind_param($stmt, "s", $usuario);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+
+        if (mysqli_stmt_num_rows($stmt) > 0) {
+            echo "Error: El usuario ya existe.";
+        } else {
+            // Insertar el nuevo usuario
+            $insert = mysqli_prepare($link, "INSERT INTO usuarios (nombreUsuario, correoElectronico, usuario, rol) VALUES (?, ?, ?, ?)");
+            mysqli_stmt_bind_param($insert, "ssss", $nombreUsuario, $correoElectronico, $usuario, $rol);
+            if (mysqli_stmt_execute($insert)) {
+                echo "Usuario creado exitosamente";
+            } else {
+                echo "Error al crear usuario: " . mysqli_error($link);
+            }
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_close($link);
     } else {
         echo "Todos los campos son requeridos";
     }
