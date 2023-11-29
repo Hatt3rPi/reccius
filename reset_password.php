b35a54effcde022f90d8fe0beb894c295157e8db
Hatt3rPi <fabarca212@gmail.com>
2023-11-28 22:42:48 -0300

    Update reset_password.php

--- reset_password.php
+++ reset_password.php
@@ -14,54 +14,60 @@ $success = '';
 $mostrarFormulario = false;
 $usuario_id = null;
 
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['token']) && !empty($_GET['token'])) {
-    $token = limpiarDato($_GET['token']);
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET['token']) && !empty($_GET['token'])) {
+        $token = limpiarDato($_GET['token']);
+        $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-    if (!$link) {
-        die("Conexión fallida: " . mysqli_connect_error());
-    }
+        if (!$link) {
+            die("Conexión fallida: " . mysqli_connect_error());
+        }
 
-    $stmt = mysqli_prepare($link, "SELECT usuario_id, fecha_expiracion FROM tokens_reset WHERE token = ? AND fecha_expiracion > NOW()");
-    mysqli_stmt_bind_param($stmt, "s", $token);
-    mysqli_stmt_execute($stmt);
-    $resultado = mysqli_stmt_get_result($stmt);
+        $stmt = mysqli_prepare($link, "SELECT usuario_id, fecha_expiracion FROM tokens_reset WHERE token = ? AND fecha_expiracion > NOW()");
+        mysqli_stmt_bind_param($stmt, "s", $token);
+        mysqli_stmt_execute($stmt);
+        $resultado = mysqli_stmt_get_result($stmt);
 
-    if ($fila = mysqli_fetch_assoc($resultado)) {
-        $mostrarFormulario = true;
-        $usuario_id = $fila['usuario_id'];
+        if ($fila = mysqli_fetch_assoc($resultado)) {
+            $mostrarFormulario = true;
+            $usuario_id = $fila['usuario_id'];
+        } else {
+            $error = 'El enlace de restablecimiento no es válido o ha expirado.';
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_close($link);
     } else {
-        $error = 'El enlace de restablecimiento no es válido o ha expirado.';
+        $error = 'Solicitud no válida.';
     }
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($link);
-} else {
-    $error = 'Solicitud no válida.';
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reset_password'])) {
     $nuevaContrasena = limpiarDato($_POST['nuevaContrasena']);
-    $usuario_id = limpiarDato($_POST['usuario_id']) ?? null; // Obtener usuario_id del campo oculto
+    $usuario_id = $_POST['usuario_id'] ?? null; // Obtener usuario_id del campo oculto
 
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    if (!$link) {
-        die("Conexión fallida: " . mysqli_connect_error());
-    }
+    if ($usuario_id) {
+        $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if (!$link) {
+            die("Conexión fallida: " . mysqli_connect_error());
+        }
 
-    $contrasenaHash = password_hash($nuevaContrasena, PASSWORD_DEFAULT);
-    $update = mysqli_prepare($link, "UPDATE usuarios SET contrasena = ? WHERE id = ?");
-    mysqli_stmt_bind_param($update, "si", $contrasenaHash, $usuario_id);
+        $contrasenaHash = password_hash($nuevaContrasena, PASSWORD_DEFAULT);
+        $update = mysqli_prepare($link, "UPDATE usuarios SET contrasena = ? WHERE id = ?");
+        mysqli_stmt_bind_param($update, "si", $contrasenaHash, $usuario_id);
 
-    if (mysqli_stmt_execute($update)) {
-        $success = 'Tu contraseña ha sido restablecida exitosamente.';
-        // Opcionalmente, invalidar el token aquí
+        if (mysqli_stmt_execute($update)) {
+            $success = 'Tu contraseña ha sido restablecida exitosamente.';
+            // Opcionalmente, invalidar el token aquí
+        } else {
+            $error = 'Error al restablecer tu contraseña: ' . mysqli_stmt_error($update);
+        }
+
+        mysqli_stmt_close($update);
+        mysqli_close($link);
     } else {
-        $error = 'Error al restablecer tu contraseña: ' . mysqli_stmt_error($update);
+        $error = 'Error al procesar la solicitud.';
     }
-
-    mysqli_stmt_close($update);
-    mysqli_close($link);
 }
 
 ?>
