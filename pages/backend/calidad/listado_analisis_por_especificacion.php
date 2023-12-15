edd5c3438660b762bb3b988a5c112575ae6d4d5c
Hatt3rPi <fabarca212@gmail.com>
2023-12-15 00:04:37 -0300

    Update listado_analisis_por_especificacion.php

--- pages/backend/calidad/listado_analisis_por_especificacion.php
+++ pages/backend/calidad/listado_analisis_por_especificacion.php
@@ -2,8 +2,10 @@
 session_start();
 require_once "/home/customw2/conexiones/config_reccius.php";
 
-$id=$_GET['token'];
-// Consulta para obtener las especificaciones de productos
+// Validación y saneamiento del ID
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+// Preparar la consulta
 $stmt = mysqli_prepare($link, "SELECT 
 can.id_analisis, 
 can.id_especificacion_producto, 
@@ -15,23 +17,29 @@ can.criterios_aceptacion
 FROM calidad_productos as cp 
 INNER JOIN calidad_especificacion_productos as cep ON cp.id = cep.id_producto 
 INNER JOIN calidad_analisis as can on cep.id_especificacion=can.id_especificacion_producto
-WHERE cp.id=?;");
+WHERE cp.id = ?");
+
+// Enlazar parámetro
 mysqli_stmt_bind_param($stmt, "i", $id);
 
+// Ejecutar la consulta
+mysqli_stmt_execute($stmt);
 
-$result = mysqli_stmt_execute($stmt);
+// Obtener resultados
+$result = mysqli_stmt_get_result($stmt);
 
 $data = [];
 
-if ($result && $result->num_rows > 0) {
-    // Recorrer los resultados y añadirlos al array de datos
-    while ($row = $result->fetch_assoc()) {
+// Verificar si hay resultados
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
         $data[] = $row;
     }
 }
 
-// Cerrar la conexión a la base de datos
-$link->close();
+// Cerrar el statement y la conexión
+mysqli_stmt_close($stmt);
+mysqli_close($link);
 
 // Enviar los datos en formato JSON
 header('Content-Type: application/json');
