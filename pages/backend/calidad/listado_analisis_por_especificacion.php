94596c1cab4137774bc690174cd91e14b2b19fc4
Hatt3rPi <fabarca212@gmail.com>
2023-12-17 21:05:34 -0300

    preparacion carga info

--- pages/backend/calidad/listado_analisis_por_especificacion.php
+++ pages/backend/calidad/listado_analisis_por_especificacion.php
@@ -4,44 +4,60 @@ require_once "/home/customw2/conexiones/config_reccius.php";
 
 // Validación y saneamiento del ID
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-$analisis = isset($_GET['analisis']) ? intval($_GET['analisis']) : 0;
-// Preparar la consulta
-$stmt = mysqli_prepare($link, "SELECT 
-can.id_analisis, 
-can.id_especificacion_producto, 
-cp.id as id_producto, 
-can.tipo_analisis, 
-can.descripcion_analisis, 
-can.metodologia, 
-can.criterios_aceptacion 
-FROM calidad_productos as cp 
-INNER JOIN calidad_especificacion_productos as cep ON cp.id = cep.id_producto 
-INNER JOIN calidad_analisis as can on cep.id_especificacion=can.id_especificacion_producto
-WHERE cp.id = ? and tipo_analisis= ? ;");
 
-// Enlazar parámetro
-mysqli_stmt_bind_param($stmt, "is", $id, $analisis);
+// Consulta para obtener los productos, especificaciones y análisis asociados
+$query = "SELECT 
+            cp.id as id_producto,
+            cep.id_especificacion,
+            cep.estado, 
+            cep.documento, 
+            cp.nombre_producto AS producto, 
+            cp.tipo_producto, 
+            cp.concentracion, 
+            cp.formato, 
+            cp.elaborado_por, 
+            cep.fecha_expiracion,
+            can.id_analisis, 
+            can.tipo_analisis, 
+            can.descripcion_analisis, 
+            can.metodologia, 
+            can.criterios_aceptacion 
+        FROM calidad_productos as cp
+        INNER JOIN calidad_especificacion_productos as cep ON cp.id = cep.id_producto
+        LEFT JOIN calidad_analisis as can ON cep.id_especificacion = can.id_especificacion_producto
+        WHERE cp.id = ?";
 
-// Ejecutar la consulta
+$stmt = mysqli_prepare($link, $query);
+mysqli_stmt_bind_param($stmt, "i", $id);
 mysqli_stmt_execute($stmt);
-
-// Obtener resultados
 $result = mysqli_stmt_get_result($stmt);
 
 $data = [];
+$especificaciones = [];
 
-// Verificar si hay resultados
-if ($result && mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = $row;
+while ($row = mysqli_fetch_assoc($result)) {
+    // Agrupar por especificación
+    $especificacion_id = $row['especificacion_id'];
+    if (!isset($especificaciones[$especificacion_id])) {
+        $especificaciones[$especificacion_id] = [
+            'especificacion' => array_intersect_key($row, array_flip(['estado', 'documento', 'fecha_expiracion'])),
+            'analisis' => []
+        ];
+    }
+    // Agregar análisis a la especificación correspondiente
+    if ($row['id_analisis']) {
+        $especificaciones[$especificacion_id]['analisis'][] = array_intersect_key($row, array_flip(['id_analisis', 'tipo_analisis', 'descripcion_analisis', 'metodologia', 'criterios_aceptacion']));
     }
 }
 
-// Cerrar el statement y la conexión
+// Reorganizar los datos para la salida
+foreach ($especificaciones as $espec) {
+    $data[] = $espec;
+}
+
 mysqli_stmt_close($stmt);
 mysqli_close($link);
 
-// Enviar los datos en formato JSON
 header('Content-Type: application/json');
 echo json_encode(['data' => $data]);
 ?>
