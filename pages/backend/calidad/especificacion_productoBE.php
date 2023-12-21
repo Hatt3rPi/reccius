5330209c054dbd8793e15733f1695d5a1c2db19e
Hatt3rPi <fabarca212@gmail.com>
2023-12-21 15:46:24 -0300

    Update especificacion_productoBE.php

--- pages/backend/calidad/especificacion_productoBE.php
+++ pages/backend/calidad/especificacion_productoBE.php
@@ -60,14 +60,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($stmt) {
             mysqli_stmt_bind_param($stmt, "ssssss", $producto, $tipoProducto, $concentracion, $formato, $elaboradoPor, $numeroDocumento);
             $crea_producto=mysqli_stmt_execute($stmt);
-            
+            $idProducto = mysqli_insert_id($link);
             //in trazabilidad
             $resultado = $crea_producto ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
             $error = $crea_producto ? null :  "Error al ejecutar la consulta: " . mysqli_stmt_error($stmt);
-            registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea producto', 'calidad_productos',  mysqli_insert_id($link), $query, [$producto, $tipoProducto, $concentracion, $formato, $elaboradoPor, $numeroDocumento], $resultado, $error);
+            registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea producto', 'calidad_productos',  $idProducto, $query, [$producto, $tipoProducto, $concentracion, $formato, $elaboradoPor, $numeroDocumento], $resultado, $error);
             // out trazabidad
             if ($crea_producto) {
-                $idProducto = mysqli_insert_id($link);
+                
                 $fechaEdicionDateTime = new DateTime($fechaEdicion);
                 $fechaEdicionDateTime->modify("+$vigencia years");
                 $fechaExpiracion = $fechaEdicionDateTime->format('Y-m-d');
@@ -77,17 +77,74 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if ($stmt2) {
                     mysqli_stmt_bind_param($stmt2, "issssi", $idProducto, $numeroDocumentoFormateado, $fechaEdicion, $version, $fechaExpiracion, $vigencia);
                     $crea_especificacion=mysqli_stmt_execute($stmt2);
+                    $idEspecificacion = mysqli_insert_id($link); // ID de la especificación insertada
                     //in trazabilidad
                         $resultado = $crea_especificacion ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
                         $error = $resultado ? null : "Error al ejecutar la consulta: " . mysqli_stmt_error($stmtAnalisisFQ);
-                        registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea especificación', 'calidad_calidad_especificacion_productosnalisis',  mysqli_insert_id($link), $query_especificacion, [$idProducto, $numeroDocumentoFormateado, $fechaEdicion, $version, $fechaExpiracion, $vigencia], $resultado, $error);
+                        registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea especificación', 'calidad_especificacion_productos',  $idEspecificacion, $query_especificacion, [$idProducto, $numeroDocumentoFormateado, $fechaEdicion, $version, $fechaExpiracion, $vigencia], $resultado, $error);
                     // out trazabidad
 
                     if ($crea_especificacion) {
                         //echo "Especificación de producto creada con éxito.";
                         $exito=true;
-                        $idEspecificacion = mysqli_insert_id($link); // ID de la especificación insertada
+                        
                         $mensaje = "Especificación de producto creada con éxito.";
+                        if (isset($_POST['analisisFQ']) && is_array($_POST['analisisFQ'])) {
+                            foreach ($_POST['analisisFQ'] as $analisis) {
+                                $crea_analisis='';
+                                // Asegúrate de que estas claves coincidan con las de tu array
+                                $descripcion_analisis = limpiarDato($analisis['descripcion_analisis']);
+                                $metodologia = limpiarDato($analisis['metodologia']);
+                                $criterios_aceptacion = limpiarDato($analisis['criterio']);
+                                $tipo='analisis_FQ';
+                                $query_analisis="INSERT INTO calidad_analisis (id_especificacion_producto, tipo_analisis, descripcion_analisis, metodologia, criterios_aceptacion) VALUES (?, ?, ?, ?, ?)";
+                                $stmtAnalisisFQ = mysqli_prepare($link, $query_analisis);
+                                mysqli_stmt_bind_param($stmtAnalisisFQ, "issss", $idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion);
+                                $crea_analisis=mysqli_stmt_execute($stmtAnalisisFQ);
+                                $id_analisis=mysqli_insert_id($link);
+                                //in trazabilidad
+                                    $resultado = $crea_analisis ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
+                                    $error = $resultado ? null : "Error al ejecutar la consulta: " . mysqli_stmt_error($stmtAnalisisFQ);
+                                    registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea analisis FQ', 'calidad_analisis',  $id_analisis, $query_analisis, [$idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion], $resultado, $error);
+                                // out trazabidad
+                                if ($crea_analisis) {
+                                    // Éxito en la inserción
+                                } else {
+                                    $mensaje = "Error al insertar en calidad_analisis para analisis_FQ: " . mysqli_error($link);
+                                }
+                                mysqli_stmt_close($stmtAnalisisFQ);
+                            }
+                        }
+                    
+                        // Procesar datos de analisisMB
+                        if (isset($_POST['analisisMB']) && is_array($_POST['analisisMB'])) {
+                            foreach ($_POST['analisisMB'] as $analisis) {
+                                // Asegúrate de que estas claves coincidan con las de tu array
+                                $crea_analisis='';
+                                $descripcion_analisis = limpiarDato($analisis['descripcion_analisis']);
+                                $metodologia = limpiarDato($analisis['metodologia']);
+                                $criterios_aceptacion = limpiarDato($analisis['criterio']);
+                                $tipo='analisis_MB';
+                                $query_analisis="INSERT INTO calidad_analisis (id_especificacion_producto, tipo_analisis, descripcion_analisis, metodologia, criterios_aceptacion) VALUES (?, ?, ?, ?, ?)";
+                                $stmtAnalisisMB = mysqli_prepare($link, $query_analisis);
+                                mysqli_stmt_bind_param($stmtAnalisisMB, "issss", $idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion);
+                                $crea_analisis = mysqli_stmt_execute($stmtAnalisisMB);
+                                $id_analisis=mysqli_insert_id($link);
+                                //in trazabilidad
+                                    $resultado = $crea_analisis ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
+                                    $error = $crea_analisis ? null: "Error al ejecutar la consulta: " . mysqli_stmt_error($stmtAnalisisMB);
+                                    registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea analisis MB', 'calidad_analisis',  $id_analisis, $query_analisis, [$idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion], $resultado, $error);
+                                // out trazabidad
+                                if ($crea_analisis) {
+                                    // Éxito en la inserción
+                                } else {
+                                    $mensaje = "Error al insertar en calidad_analisis para analisis_MB: " . mysqli_error($link);
+                                }
+                                mysqli_stmt_close($stmtAnalisisMB);
+                            }
+                        }
+
+
                     } else {
                         $mensaje = "Error al insertar en calidad_especificacion_productos: " . mysqli_error($link);
                     }
@@ -108,58 +165,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Procesar datos de analisisFQ
     // Procesar datos de analisisFQ
-    if (isset($_POST['analisisFQ']) && is_array($_POST['analisisFQ'])) {
-        foreach ($_POST['analisisFQ'] as $analisis) {
-            $crea_analisis='';
-            // Asegúrate de que estas claves coincidan con las de tu array
-            $descripcion_analisis = limpiarDato($analisis['descripcion_analisis']);
-            $metodologia = limpiarDato($analisis['metodologia']);
-            $criterios_aceptacion = limpiarDato($analisis['criterio']);
-            $tipo='analisis_FQ';
-            $query_analisis="INSERT INTO calidad_analisis (id_especificacion_producto, tipo_analisis, descripcion_analisis, metodologia, criterios_aceptacion) VALUES (?, ?, ?, ?, ?)";
-            $stmtAnalisisFQ = mysqli_prepare($link, $query_analisis);
-            mysqli_stmt_bind_param($stmtAnalisisFQ, "issss", $idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion);
-            $crea_analisis=mysqli_stmt_execute($stmtAnalisisFQ);
-            //in trazabilidad
-                $resultado = $crea_analisis ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
-                $error = $resultado ? null : "Error al ejecutar la consulta: " . mysqli_stmt_error($stmtAnalisisFQ);
-                registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'CALIDAD - crea analisis FQ', 'calidad_analisis',  mysqli_insert_id($link), $query_analisis, [$idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion], $resultado, $error);
-            // out trazabidad
-            if ($crea_analisis) {
-                // Éxito en la inserción
-            } else {
-                $mensaje = "Error al insertar en calidad_analisis para analisis_FQ: " . mysqli_error($link);
-            }
-            mysqli_stmt_close($stmtAnalisisFQ);
-        }
-    }
 
-    // Procesar datos de analisisMB
-    if (isset($_POST['analisisMB']) && is_array($_POST['analisisMB'])) {
-        foreach ($_POST['analisisMB'] as $analisis) {
-            // Asegúrate de que estas claves coincidan con las de tu array
-            $crea_analisis='';
-            $descripcion_analisis = limpiarDato($analisis['descripcion_analisis']);
-            $metodologia = limpiarDato($analisis['metodologia']);
-            $criterios_aceptacion = limpiarDato($analisis['criterio']);
-            $tipo='analisis_MB';
-            $query_analisis="INSERT INTO calidad_analisis (id_especificacion_producto, tipo_analisis, descripcion_analisis, metodologia, criterios_aceptacion) VALUES (?, ?, ?, ?, ?)";
-            $stmtAnalisisMB = mysqli_prepare($link, $query_analisis);
-            mysqli_stmt_bind_param($stmtAnalisisMB, "issss", $idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion);
-            $crea_analisis = mysqli_stmt_execute($stmtAnalisisMB);
-            //in trazabilidad
-                $resultado = $crea_analisis ? 1 : 0; // Suponiendo que 1 es éxito y 0 es fracaso
-                $error = $crea_analisis ? null: "Error al ejecutar la consulta: " . mysqli_stmt_error($stmtAnalisisMB);
-                registrarTrazabilidad($_SESSION['usuario'], $_SERVER['PHP_SELF'], 'crea analisis MB', 'calidad_analisis',  mysqli_insert_id($link), $query_analisis, [$idEspecificacion, $tipo, $descripcion_analisis, $metodologia, $criterios_aceptacion], $resultado, $error);
-            // out trazabidad
-            if ($crea_analisis) {
-                // Éxito en la inserción
-            } else {
-                $mensaje = "Error al insertar en calidad_analisis para analisis_MB: " . mysqli_error($link);
-            }
-            mysqli_stmt_close($stmtAnalisisMB);
-        }
-    }
 
     mysqli_close($link);
 } else {
