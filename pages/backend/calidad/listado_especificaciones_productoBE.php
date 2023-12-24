b98365128e0a5934018e04ac0ba3adfecb3941f1
H4tt3r <fabarca212@gmail.com>
2023-12-23 21:50:48 -0300

    Update listado_especificaciones_productoBE.php

--- pages/backend/calidad/listado_especificaciones_productoBE.php
+++ pages/backend/calidad/listado_especificaciones_productoBE.php
@@ -14,7 +14,7 @@ $query = "SELECT
             cp.concentracion, 
             cp.formato, 
             cp.elaborado_por,
-            cp.pais_origen 
+            cp.pais_origen,
             cep.fecha_expiracion 
         FROM calidad_productos as cp
         INNER JOIN calidad_especificacion_productos as cep ON cp.id = cep.id_producto;";
