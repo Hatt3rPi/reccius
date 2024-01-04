b975a3f54002f14ae6383a2599feda760e48384b
Hatt3rPi <fabarca212@gmail.com>
2024-01-03 23:13:41 -0300

    Update especificacion_producto.php

--- pages/especificacion_producto.php
+++ pages/especificacion_producto.php
@@ -222,7 +222,7 @@ while ($row = mysqli_fetch_assoc($result)) {
             <div class="actions-container">
                 <button type="button" id="guardar" name="guardar" class="action-button">Guardar Especificación</button>
                 <button type="button" id="editarGenerarVersion" name="editarGenerarVersion" class="action-button" style="background-color: red; color: white;display: none;">Editar y generar nueva versión</button>
-                <input type="text" name="id_producto" style="display: none;">
+                <input type="text" id="id_producto" name="id_producto" style="display: none;">
             </div>
         </form>
     </div>
