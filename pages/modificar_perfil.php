d2819bbec38a5589cd7351bd1ff41a83d4364695
Hatt3rPi <fabarca212@gmail.com>
2024-01-09 22:49:30 -0300

    p4

--- pages/modificar_perfil.php
+++ pages/modificar_perfil.php
@@ -26,8 +26,9 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 enctype="multipart/form-data">
 
                 <!-- Sección de Cambio de Contraseña -->
-                <fieldset>
-                    <legend>Cambio de Contraseña</legend>
+                <div class="seccion">
+
+                    <h3>Cambio de Contraseña</h3>
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="checkbox" id="switch_contrasena" onclick="toggleInputs('switch_contrasena')">
                         <label class="form-check-label" for="switch_contrasena">Editar</label>
@@ -46,9 +47,9 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                             <input class="switch_contrasena" type="password" id="confirmarPassword" name="confirmarPassword" disabled >
                         </div>
                         
-                </fieldset>
-                <fieldset>
-                    <legend>Cambio de Foto de Perfil</legend>
+                </div>
+                <div class="seccion">
+                    <h3>Cambio de Foto de Perfil</h3>
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" id="switch_foto" onclick="toggleInputs('switch_foto')">
                             <label class="form-check-label" for="switch_foto">Editar</label>
@@ -58,9 +59,10 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                             <label for="fotoPerfil">Foto de Perfil:</label>
                             <input  class="switch_foto" type="file" id="fotoPerfil" name="fotoPerfil" accept="image/*" disabled >
                         </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Editar información de Usuario</legend>
+                </div>
+                <div class="seccion">
+
+                    <h3>Editar información de Usuario</h3>
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" id="switch_info" onclick="toggleInputs('switch_info')">
                             <label class="form-check-label" for="switch_info">Editar</label>
@@ -78,9 +80,10 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                             <label for="nombre_corto">Nombre y Cargo Abreviado:</label>
                             <input class="switch_info" type="text" id="nombre_corto" name="nombre_corto" disabled>
                         </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Certificado Nacional de Prestadores Individuales de Salud</legend>
+                </div>
+                <div class="seccion">
+
+                    <h3>Certificado Nacional de Prestadores Individuales de Salud</h3>
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" id="switch_certificado" onclick="toggleInputs('switch_certificado')">
                             <label class="form-check-label" for="switch_certificado">Editar</label>
@@ -90,7 +93,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                             <label for="certificado">Cargar Documento (extraído desde https://rnpi.superdesalud.gob.cl/):</label>
                             <input class="switch_certificado" type="file" id="certificado" name="certificado" accept="application/pdf" disabled>
                         </div>
-                </fieldset>          
+                                            </div>          
                 <input type="hidden" name="usuario" value="<?php echo $_SESSION['usuario']; ?>">      
                 <button type="submit" name="modificarPerfil">Modificar Perfil</button>
             </form>
