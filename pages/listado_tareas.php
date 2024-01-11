2fd7979f47e8a3ec9d1c58fbf7acf03d9fa9f22f
Hatt3rPi <fabarca212@gmail.com>
2024-01-10 23:59:56 -0300

    Update listado_tareas.php

--- pages/listado_tareas.php
+++ pages/listado_tareas.php
@@ -121,7 +121,9 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             // Agrega acciones según el estado de la tarea
             if (d.estado === 'Activo' || d.estado === 'Atrasado' || d.estado === 'Fecha de Vencimiento cercano') {
                 acciones += '<button class="accion-btn" title="Recordar Tarea" id="' + d.id + '" name="recordar" onclick="botones(this.id, this.name, \'tareas\')" ><i class="fas fa-envelope"></i></button><a> </a>';
-                acciones += '<button class="accion-btn" title="Cambiar Usuario Ejecutor" id="' + d.id + '" name="cambiar_usuario" onclick="botones(this.id, this.name, \'tareas\')" ><i class="fas fa-user-edit"></i></button><a> </a>';
+                if (d.usuario_ejecutor === usuarioActual) {
+                    acciones += '<button class="accion-btn" title="Cambiar Usuario Ejecutor" id="' + d.id + '" name="cambiar_usuario" onclick="botones(this.id, this.name, \'tareas\')" ><i class="fas fa-user-edit"></i></button><a> </a>';
+                }
                 if (d.usuario_ejecutor === usuarioActual) {
                     acciones += '<button class="accion-btn" title="Finalizar Tarea" id="' + d.id + '" name="finalizar" onclick="botones(this.id, this.name, \'tareas\')"><i class="fas fa-check"></i></button>';
                 }
