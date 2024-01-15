428a2ea64ff230943b37e77232197bce06324d76
Hatt3rPi <fabarca212@gmail.com>
2024-01-14 21:59:59 -0300

    Update notificaciones.php

--- pages/backend/login/notificaciones.php
+++ pages/backend/login/notificaciones.php
@@ -5,7 +5,7 @@ require_once "/home/customw2/conexiones/config_reccius.php";
 // Asumiendo que tienes el ID de usuario en la sesión
 $user = $_SESSION['usuario'];
 
-$query = "SELECT COUNT(*) AS count FROM tareas WHERE estado in ('Activo', 'Atrasado', 'Fecha de Vencimiento cercano') AND usuario_ejecutor = ?";
+$query = "SELECT COUNT(*) AS count FROM tareas WHERE estado in ('Activo', 'Atrasado', 'Fecha de Vencimiento cercano', 'Activa', 'Atrasada') AND usuario_ejecutor = ?";
 $stmt = $link->prepare($query);
 $stmt->bind_param("s", $user);
 $stmt->execute();
