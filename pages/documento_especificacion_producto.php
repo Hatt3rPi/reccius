5c9e0abc680c9cc74c6e67914ffcf087f24bb069
lucifxr63 <lucianoalonso2000@gmail.com>
2024-01-22 19:20:13 -0300

    update

--- pages/documento_especificacion_producto.php
+++ pages/documento_especificacion_producto.php
@@ -645,22 +645,11 @@
         window.onload = function() {
             cargarDatosEspecificacion(id);
             verificarYMostrarBotonFirma();
-
-            // Crear un contenedor para envolver ambos form-container
-            var wrapper = document.createElement('div');
-            wrapper.className = 'container-wrapper';
-
-            // Seleccionar el contenedor existente
-            var existingContainer = document.getElementById('form-container');
-
-            // Clonar el contenedor existente
-            var clonedContainer = existingContainer.cloneNode(true);
-
-            // Insertar el contenedor clonado en el DOM
-            existingContainer.parentNode.insertBefore(wrapper, existingContainer);
-            wrapper.appendChild(existingContainer);
-            wrapper.appendChild(clonedContainer);
         };
+        document.addEventListener("DOMContentLoaded", function() {
+        var bodyContent = document.body.innerHTML;
+        document.body.innerHTML += bodyContent;
+         });
 
     </script>
 </body>
