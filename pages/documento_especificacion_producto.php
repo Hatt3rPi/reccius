633b3520ef6abb9778dcf19f1860a8b5e8ed68f9
lucifxr63 <lucianoalonso2000@gmail.com>
2024-02-08 19:20:59 -0300

    Update documento_especificacion_producto.php

--- pages/documento_especificacion_producto.php
+++ pages/documento_especificacion_producto.php
@@ -700,16 +700,16 @@
             mainTablas.appendChild(newContentDiv); // Añadir 'content' a 'maintablas'
 
             // Crear y agregar la sección de análisis para 'content'
-            const analysisSectionContent = createAnalysisSection("I. Análisis Generales");
-            newContentDiv.appendChild(analysisSectionContent);
+            //const analysisSectionContent = createAnalysisSection("I. Análisis Generales");
+            //newContentDiv.appendChild(analysisSectionContent);
 
             const newAdditionalContentDiv = createEl("div");
             newAdditionalContentDiv.id = "additionalContent";
             mainTablas.appendChild(newAdditionalContentDiv); // Añadir 'additionalContent' a 'maintablas'
 
             // Crear y agregar la sección de análisis para 'additionalContent'
-            const analysisSectionAdditionalContent = createAnalysisSection("II. Análisis Microbiológico");
-            newAdditionalContentDiv.appendChild(analysisSectionAdditionalContent);
+            //const analysisSectionAdditionalContent = createAnalysisSection("II. Análisis Microbiológico");
+            //newAdditionalContentDiv.appendChild(analysisSectionAdditionalContent);
 
             // Añadir 'maintablas' a 'container'
             container.appendChild(mainTablas);
