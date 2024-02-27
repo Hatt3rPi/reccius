3be63ab35bf75ce05bb2e856e8ad9f7198d6a1f5
lucifxr63 <lucianoalonso2000@gmail.com>
2024-02-26 22:27:45 -0300

    Update modificar_perfil.php

--- pages/modificar_perfil.php
+++ pages/modificar_perfil.php
@@ -213,25 +213,6 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
         }
     });
 });
-
-
-    // Función para mostrar la notificación
-    function showNotification(message, isSuccess) {
-        var notification = document.getElementById('notification');
-        var messageElement = document.getElementById('notification-message');
-        messageElement.textContent = message;
-        
-        // Añadir la clase para el estilo de éxito o error
-        notification.className = isSuccess ? 'notification-container notify success' : 'notification-container notify error';
-        
-        // Mostrar la notificación
-        notification.style.display = 'block';
-        
-        // Ocultar la notificación después de 5 segundos
-        setTimeout(function() {
-            $(notification).fadeOut();
-        }, 5000);
-    }
 });
 
 // Código para manejar los switches y cargar información existente aquí...
