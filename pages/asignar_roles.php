6a43f2951cef953e951ae90bba1721fd0b67955c
lucifxr63 <lucianoalonso2000@gmail.com>
2024-02-28 14:33:19 -0300

    Update asignar_roles.php

--- pages/asignar_roles.php
+++ pages/asignar_roles.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" >
     <title>Formulario de Permisos</title>
     <!-- Incluir Bootstrap para estilos -->
     <link rel="stylesheet" href="../assets/css/CrearUsuario.css">
