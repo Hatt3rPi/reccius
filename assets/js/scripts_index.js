7a56061a142d4f98d20484fc07a242b1ada21ae5
Hatt3rPi <fabarca212@gmail.com>
2024-02-28 22:00:22 -0300

    Update scripts_index.js

--- assets/js/scripts_index.js
+++ assets/js/scripts_index.js
@@ -611,22 +611,33 @@ function botones(id, accion, base) {
 }
 
 // Función para mostrar la notificación
-function showNotification(message, isSuccess) {
-    var notification = document.getElementById('notification');
-    var messageElement = document.getElementById('notification-message');
-    messageElement.textContent = message;
+function mostrarNotificacion(mensaje, tipoNotificacion) {
+    var notificacion = document.getElementById('notification');
+    var elementoMensaje = document.getElementById('notification-message');
+    elementoMensaje.textContent = mensaje;
     
-    // Añadir la clase para el estilo de éxito o error
-    notification.className = isSuccess ? 'notification-container notify success' : 'notification-container notify error';
+    // Determinar la clase basada en el tipo de notificación
+    switch(tipoNotificacion) {
+        case 'éxito':
+            notificacion.className = 'notification-container notify success';
+            break;
+        case 'advertencia':
+            notificacion.className = 'notification-container notify warning';
+            break;
+        case 'error':
+            notificacion.className = 'notification-container notify error';
+            break;
+        default:
+            notificacion.className = 'notification-container notify'; // Clase por defecto si el tipo no se reconoce
+    }
     
     // Mostrar la notificación
-    notification.style.display = 'block';
+    notificacion.style.display = 'block';
     
     // Ocultar la notificación después de 5 segundos
     setTimeout(function() {
-        $(notification).fadeOut();
+        $(notificacion).fadeOut();
     }, 5000);
-    }
-    
-    
+}
+
     
