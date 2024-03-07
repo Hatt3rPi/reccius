3e31ea8d8a8b0f1edea8e3fc934d2aed4bf62168
lucifxr63 <lucianoalonso2000@gmail.com>
2024-03-07 16:06:25 -0300

    rollback

--- pages/documento_especificacion_producto.php
+++ pages/documento_especificacion_producto.php
@@ -235,7 +235,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 success: function(response) {
                     procesarDatosEspecificacion(response);
 
-                   // verificarYMostrarBotonFirma(response);
+                    verificarYMostrarBotonFirma(response);
                 },
                 error: function(xhr, status, error) {
                     console.error("Error en la solicitud: ", status, error);
@@ -279,8 +279,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             setTimeout(obtenerAlturaElementosYCalcularEspacioDisponible, 100);
 
             setTimeout(ocultarContenedorPrincipal, 200);
-
-            setTimeout(verificarYMostrarBotonFirma,250);
+            
             setTimeout(actualizarContadorPaginas, 300);
 
         }
