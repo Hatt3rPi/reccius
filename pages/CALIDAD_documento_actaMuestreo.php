b6d1b38cc604ae35705cffa8cbf758322f7efbce
Hatt3rPi <fabarca212@gmail.com>
2024-04-16 21:33:28 -0400

    oculta botones

--- pages/CALIDAD_documento_actaMuestreo.php
+++ pages/CALIDAD_documento_actaMuestreo.php
@@ -982,6 +982,9 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 }
                 $('#nro_version').text(1);
                 $('#realizadoPor').text('Nombre:');
+                document.querySelectorAll('.formulario.verif, .formulario.resp').forEach(function(element) {
+                element.style.display = 'none'; // Establecer el estilo de display a 'none' para ocultarlos
+                });
             }
 
 
