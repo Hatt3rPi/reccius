7d184ed2a2624d3235f53b0e14b730e5dd39c007
Javier <javier2000asr@gmail.com>
2024-04-22 16:24:42 -0400

    add aws.phar and setup crud functions

---
+++ pages/backend/cloud/R2_manager.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+require_once "/home/customw2/conexiones/config_reccius.php";
+require_once "../../../assets/dependencies/aws.phar";
+
+use  Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
+
+if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
+  http_response_code(403);
+  echo json_encode(['error' => 'Acceso denegado']);
+  exit;
+}
+
+$bucket_name        = "reccius-bucket";
+$account_id         = "<accountid>";
+$access_key_id      = "<access_key_id>";
+$access_key_secret  = "<access_key_secret>";
+
+$credentials = new Credentials($access_key_id, $access_key_secret);
+
+$options = [
+  'region' => 'auto',
+  'endpoint' => "https://$account_id.r2.cloudflarestorage.com",
+  'version' => 'latest',
+  'credentials' => $credentials
+];
+
+$R2_client = new S3Client($options);
+
+function setFile($params) {
+  global $R2_client, $bucket_name;
+
+  if (!is_array($params)) {
+      throw new InvalidArgumentException('Se espera un arreglo.');
+  }
+  if (!isset($params['fileBinary']) || !isset($params['folder']) || !isset($params['fileName'])) {
+      throw new InvalidArgumentException('Faltan parámetros requeridos.');
+  }
+
+  // Acceder a los parámetros
+  $fileBinary = $params['fileBinary'];
+  $folder = $params['folder'];
+  $fileName = $params['fileName'];
+
+  try {
+    $result = $R2_client->putObject([
+      'Bucket' => $bucket_name,
+      'Key' => $folder . '/' . $fileName,
+      'Body' => $fileBinary,
+      'ACL' => 'private'
+    ]);
+      return json_encode(['success' => $result, 'error' => '']);
+
+  } catch (Aws\Exception\AwsException $e) {
+      return json_encode(['success'=> false, 'error' => $e->getMessage()]);
+  }
+
+}
+
+function deleteFile($folder, $fileName) {
+  global $R2_client, $bucket_name;
+
+  try {
+      $result = $R2_client->deleteObject([
+          'Bucket' => $bucket_name,
+          'Key' => $folder . '/' . $fileName
+      ]);
+      return json_encode(['success' => true, 'error' => '']);
+  } catch (Aws\Exception\AwsException $e) {
+      return json_encode(['success' => false, 'error' => $e->getMessage()]);
+  }
+}
+
+?>
