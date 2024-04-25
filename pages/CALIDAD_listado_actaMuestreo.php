37eb7e3afbfd57247f008c9027e077e5ab105e47
lucifxr63 <lucianoalonso2000@gmail.com>
2024-04-25 15:18:50 -0400

    Revert "fix buttons analisis to acta"
    This reverts commit 4c42d9ffb567a8c3f39d877a0f398295d8d77400.

--- pages/CALIDAD_listado_actaMuestreo.php
+++ pages/CALIDAD_listado_actaMuestreo.php
@@ -177,10 +177,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
 
             // Botón para revisar siempre presente
             acciones += '<button class="accion-btn" title="WIP Ingresar resultados Acta Muestreo" type="button" id="' + d.id_acta + '" name="resultados_actaMuestreo" onclick="botones(' + d.id_acta + ', this.name, \'laboratorio\')"><i class="fas fa-search"></i></button><a> </a>';
-            acciones += '<button class="accion-btn" title="WIP Generar Documento" id="' + d.id_analisisExterno + '" name="generar_documento_solicitudes" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i></button><a> </a>';
-            if (d.muestreado_por === usuarioActual) {
-                acciones += '<button class="accion-btn" title="Generar Acta de muestreo" id="' + d.id_analisisExterno + '" name="generar_acta_muestreo" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fas fa-check"></i></button>';
-            }
+            acciones += '<button class="accion-btn" title="WIP Generar Documento" id="' + d.id_acta + '" name="generar_documento_actaMuestreo" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i></button><a> </a>';
             acciones += '</td></tr></table>';
             return acciones;
         }
