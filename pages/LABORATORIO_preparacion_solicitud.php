9b418784cd028cdfbd7f0fb96dcaba7639f39656
Javier <javier2000asr@gmail.com>
2024-04-29 14:41:58 -0400

    feat id_producto disabled

--- pages/LABORATORIO_preparacion_solicitud.php
+++ pages/LABORATORIO_preparacion_solicitud.php
@@ -488,6 +488,55 @@ $fechaEntregaEstimadaFormato = $fechaEntregaEstimada->format('Y-m-d');
             console.error("No se recibieron datos válidos: ", response);
         }
     }
+    /*
+    numero_registro: DCAL-CC-ENE-003
+    version: 3
+    numero_solicitud: SAEPT-0101001-03
+    fecha_registro: 2024-06-27
+    lote: LOTE-00003
+    tamano_lote: 500
+    fecha_elaboracion: 2024-04-17
+    fecha_vencimiento: 2025-04-16
+    tipo_analisis: Análisis de rutina
+    condicion_almacenamiento: sin humedad
+    tamano_muestra: 10
+    tamano_contramuestra: 5
+    registro_isp: N°2988/18. RF XIII 06/18. 1A, 2B, 2C, 3A, 3D, 4
+    numero_pos: no
+    muestreado_por: lucianoalonso2000
+    laboratorio: CEQUC
+    otro_laboratorio: 
+    fecha_solicitud: 2024-04-26
+    analisis_segun: Cotización nueva 2
+    fecha_cotizacion: 2024-06-01
+    estandar_segun: reccius
+    hds_otro: no
+    fecha_entrega_estimada: Invalid date
+    numero_documento: 12345678
+    observaciones: Observaciones de Cotización nueva 2
+    usuario_editor: Javier Sabando
+    user_editor: javier2000asr
+    usuario_revisor: lcaques
+    id_especificacion: 2
+
+numero_registro: DCAL-CC-ABR-003
+version: 3
+numero_solicitud: SAEPT-0101001-03
+fecha_registro: 2024-04-30
+lote: LOTE-00003
+tamano_lote: 40
+fecha_elaboracion: 2024-04-29
+fecha_vencimiento: 2025-02-13
+tipo_analisis: Análisis de rutina
+condicion_almacenamiento: 100
+tamano_muestra: 10
+tamano_contramuestra: 10
+registro_isp: N°2988/18. RF XIII 06/18. 1A, 2B, 2C, 3A, 3D, 4
+numero_pos: no
+muestreado_por: mgodoy
+: 11
+id_especificacion: 107
+    */
 
     function procesarDatosActaUpdate(response) {
 
@@ -536,7 +585,7 @@ $fechaEntregaEstimadaFormato = $fechaEntregaEstimada->format('Y-m-d');
             var arrToSetEspecificaciones = [{
                     id: 'id_producto',
                     val: analisis.prod_identificador_producto,
-                    isDisabled: true
+                    isDisabled: false
                 },
                 {
                     id: 'tipo_producto',
