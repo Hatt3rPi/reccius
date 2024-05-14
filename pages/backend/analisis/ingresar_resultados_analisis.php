b0340228cd1eb01f311e08dda70b07931fc86954
lucifxr63 <lucianoalonso2000@gmail.com>
2024-05-14 16:15:52 -0400

    UPDATE backend gpt

--- pages/backend/analisis/ingresar_resultados_analisis.php
+++ pages/backend/analisis/ingresar_resultados_analisis.php
@@ -13,15 +13,17 @@ $queryAnalisisExterno = "SELECT
                             prod.tipo_concentracion AS 'prod_tipo_concentracion', 
                             prod.concentracion AS 'prod_concentracion', 
                             prod.formato AS 'prod_formato', 
-                            prod.elaborado_por AS 'prod_elaborado_por'
-                            
+                            prod.elaborado_por AS 'prod_elaborado_por',
+                            can.id_analisis,
+                            can.tipo_analisis,
+                            can.descripcion_analisis,
+                            can.metodologia, 
+                            can.criterios_aceptacion
                         FROM calidad_analisis_externo AS an
                         JOIN calidad_productos AS prod ON an.id_producto = prod.id
+                        LEFT JOIN calidad_analisis AS can ON an.id = can.id_analisis_externo
                         WHERE an.id = ?";
 
-$queryActaMuestreo = "SELECT * FROM calidad_acta_muestreo WHERE id_analisisExterno= ?";
-
-//queryAnalisisExterno
 $stmtAnali = mysqli_prepare($link, $queryAnalisisExterno);
 mysqli_stmt_bind_param($stmtAnali, "i", $id_acta);
 mysqli_stmt_execute($stmtAnali);
@@ -34,28 +36,22 @@ if ($rowAnali = mysqli_fetch_assoc($resultAnali)) {
 }
 mysqli_stmt_close($stmtAnali);
 
-
-//queryActaMuestreo
-$analisisActaMuestreo = [];
-
+// Consulta para obtener datos de calidad_acta_muestreo (si es necesario)
+$queryActaMuestreo = "SELECT * FROM calidad_acta_muestreo WHERE id_analisisExterno= ?";
 $stmtActaMuestreo = mysqli_prepare($link, $queryActaMuestreo);
 mysqli_stmt_bind_param($stmtActaMuestreo, "i", $id_acta);
 mysqli_stmt_execute($stmtActaMuestreo);
 
+$analisisActaMuestreo = [];
 $resultActaMuestreo = mysqli_stmt_get_result($stmtActaMuestreo);
 while ($row = mysqli_fetch_assoc($resultActaMuestreo)) {
     $analisisActaMuestreo[] = $row;
-
 }
 
 mysqli_stmt_close($stmtActaMuestreo);
-
-
 mysqli_close($link);
 
 // Enviar los datos en formato JSON
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode(['Acta_Muestreo' => array_values($analisisActaMuestreo), 'analisis' => $analisis], JSON_UNESCAPED_UNICODE);
-
 ?>
- 
