87b6ef090d95e582041b244cfef22a47c71efc82
Hatt3rPi <fabarca212@gmail.com>
2024-05-13 20:44:57 -0400

    Update CALIDAD_documento_actaMuestreo.php

--- pages/CALIDAD_documento_actaMuestreo.php
+++ pages/CALIDAD_documento_actaMuestreo.php
@@ -26,6 +26,8 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
     JS de DataTables 
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="../assets/js/notify.js"></script> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/DocumentoActa.css">
 
 </head>
@@ -1125,7 +1127,7 @@ function guardar_firma(selector, etapa) {
     if (!todosSeleccionados) {
         console.log("Botones no seleccionados:", botonesNoSeleccionados.join(', '));
         //alert("Por favor, asegúrate de que todos los campos han sido seleccionados.");
-        $.notify("Existen campos imcompletos.", "warn");
+        $.notify("Existen campos incompletos.", "warn");
         return; // Detiene la función si no todos están seleccionados
     }
 
