36540db4f02acb71ae399ff0b7825e5adbbcd034
Ratapan <javier2000asr@gmail.com>
2024-06-10 23:14:42 -0400

    test de pendiente de insgreso de resultados

--- pages/LABORATORIO_listado_solicitudes.php
+++ pages/LABORATORIO_listado_solicitudes.php
@@ -172,11 +172,10 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             acciones += '<button class="accion-btn" title="WIP Revisar Análisis Externo" type="button" id="' + d.id_analisisExterno + '" name="revisar" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fas fa-search"></i></button><a> </a>';
             acciones += '<button class="accion-btn" title="WIP Generar Documento" id="' + d.id_analisisExterno + '" name="generar_documento_solicitudes" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i></button><a> </a>';
                 
-
-            if (d.estado === "Pendiente ingreso resultados laboratorio") {
-                acciones += '<button class="accion-btn" title="WIP Generar Documento" id="' + d.id_analisisExterno + '" name="generar_documento_solicitudes" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i> Ingresar resultados de Laboratorio</button><a> </a>';
-                
-            }
+            //Todo:descomentar filtro
+            //if (d.estado === "Pendiente ingreso resultados laboratorio") {
+                acciones += '<button class="accion-btn" title="WIP Generar Documento" id="' + d.id_analisisExterno + '" name="generar_documento_solicitudes" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i> Ingresar resultados de Laboratorio</button><a> </a>'; 
+            //}
             if (d.muestreado_por === usuarioActual) {
                 acciones += '<button class="accion-btn" title="Generar Acta de muestreo" id="' + d.id_analisisExterno + '" name="generar_acta_muestreo" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fas fa-check"></i> Generar Acta de Muestreo</button>';
             }
