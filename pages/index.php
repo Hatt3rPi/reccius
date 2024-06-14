69aaa6c9e7b445b3edf6e55e7c71d5e40b45ed67
Hatt3rPi <fabarca212@gmail.com>
2024-06-14 15:25:45 -0400

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -70,7 +70,7 @@ if (!isset($_SESSION['foto_firma']) || empty($_SESSION['foto_firma'])) {
         }
         
         .bs-popover-auto[x-placement^=bottom]>.arrow::after, .bs-popover-bottom>.arrow::after {
-            border-bottom-color:'#dc3545' !important;
+            border-bottom-color: #dc3545 !important;
         }
         .popover-header {
             display: flex;
