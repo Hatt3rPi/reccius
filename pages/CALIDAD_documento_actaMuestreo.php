faaa36b15fc8cc6104472e2c987c011ecf67f06e
Ratapan <javier2000asr@gmail.com>
2024-06-15 17:35:20 -0400

    upload acta document style

--- pages/CALIDAD_documento_actaMuestreo.php
+++ pages/CALIDAD_documento_actaMuestreo.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../assets/css/DocumentoActa.css">
+    <link rel="stylesheet" href="../assets/css/DocumentoActa.css?version=1">
 
 </head>
 
