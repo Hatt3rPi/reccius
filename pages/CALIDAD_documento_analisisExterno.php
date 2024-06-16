23a3c56c45f5b73917616ff216cb6ceb1b615fd8
Ratapan <javier2000asr@gmail.com>
2024-06-16 01:07:59 -0400

    upload analisis_externo

--- pages/CALIDAD_documento_analisisExterno.php
+++ pages/CALIDAD_documento_analisisExterno.php
@@ -316,6 +316,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
     <button class="botones" id="revisar">Revisar</button>
     <button class="botones" id="Cambiante">cambio</button>
     <button class="botones" id="download-pdf">Descargar PDF</button>
+    <button class="botones" id="upload-pdf">Guardar PDF</button>
 </div>
 
 <div id="notification" class="notification-container notify" style="display: none;">
@@ -395,6 +396,86 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 $.notify("PDF generado con éxito", "success");
             });
     });
+    
+    document.getElementById('upload-pdf').addEventListener('click', function () {
+    const { jsPDF } = window.jspdf;
+    const pdf = new jsPDF('p', 'mm', [279, 216]);
+    const pageHeight = 279;
+    const margin = 5;
+    let currentY = margin;
+
+    const addSectionToPDF = (sectionId, yOffset = currentY, addNewPage = false) => {
+        const elementToExport = document.getElementById(sectionId);
+        if (elementToExport) {
+            elementToExport.style.border = 'none';
+            elementToExport.style.boxShadow = 'none';
+
+            return html2canvas(elementToExport, {
+                scale: 2
+            }).then(canvas => {
+                const imgData = canvas.toDataURL('image/jpeg', 1.0);
+                const imgWidth = 216 - 2 * margin;
+                const imgHeight = canvas.height * imgWidth / canvas.width;
+
+                if (addNewPage) {
+                    pdf.addPage();
+                    currentY = margin;
+                }
+
+                pdf.addImage(imgData, 'JPEG', margin, yOffset, imgWidth, imgHeight);
+                currentY = yOffset + imgHeight + margin;
+            });
+        } else {
+            return Promise.resolve();
+        }
+    };
+
+    const distributeHeight = (totalHeight, numberOfSections) => {
+        return (totalHeight - (margin * (numberOfSections + 1))) / numberOfSections;
+    };
+
+    const availableHeight = pageHeight - (2 * margin + 50);
+    const sectionHeight = distributeHeight(availableHeight, 3);
+
+    addSectionToPDF('header-container')
+        .then(() => addSectionToPDF('section1', currentY, false, sectionHeight))
+        .then(() => addSectionToPDF('section2', currentY, false, sectionHeight))
+        .then(() => addSectionToPDF('section4', currentY, false, sectionHeight))
+        .then(() => addSectionToPDF('footer-container', pageHeight - 50))
+        .then(() => addSectionToPDF('header-container', margin, true))
+        .then(() => addSectionToPDF('section3', currentY))
+        .then(() => addSectionToPDF('footer-container', pageHeight - 50))
+        .then(() => {
+            const nombreProducto = document.getElementById('nombre_producto').textContent.trim();
+            const nombreDocumento = document.getElementById('numero_registro').textContent.trim();
+            const fileName = `${nombreDocumento} ${nombreProducto}.pdf`;
+
+            pdf.output('blob').then(blob => {
+                const formData = new FormData();
+                formData.append('certificado', blob, fileName);
+                formData.append('type', 'analisis_externo');
+                formData.append('id_solicitud', idAnalisisExterno_acta); // Asegúrate de que idAnalisisExterno_acta esté definido
+
+                fetch('./backend/calidad/add_documentos.php', {
+                    method: 'POST',
+                    body: formData
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.status === 'success') {
+                            $.notify("PDF subido con éxito", "success");
+                        } else {
+                            $.notify("Error al subir el PDF: " + data.message, "error");
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        $.notify("Error al subir el PDF", "error");
+                    });
+            });
+        });
+});
+
 
     // Agregar el evento click al botón con id 'Cambiante'
     document.getElementById('Cambiante').addEventListener('click', function() {
