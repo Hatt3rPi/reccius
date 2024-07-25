f1a81a640bbd8592f1166c8c216d7a905fc4689e
lucifxr63 <lucianoalonso2000@gmail.com>
2024-07-25 16:55:08 -0400

    Update index_administrador.php

--- pages/index_administrador.php
+++ pages/index_administrador.php
@@ -26,19 +26,23 @@
             <div class="grid-item calendario">
                 <?php include 'components/index/calendario.php'; ?>
             </div>
-            <div class="section-title" style="grid-column: 1 / -1;">DISPONIBILIDAD DE PRODUCTOS</div>
+            <div class="section-title disponibilidad-productos">
+                DISPONIBILIDAD DE PRODUCTOS
+            </div>
             <div class="grid-item cartas">
                 <?php include 'components/index/cartas.php'; ?>
             </div>
-            
+
             <div class="grid-item grafo1">
-            <h2>Laboratorios Presentes</h2>    
+                <h2>Laboratorios Presentes</h2>
                 <?php include 'components/index/laboratorios.php'; ?>
             </div>
             <div class="grid-item grafo2">
                 <?php include 'components/index/productos.php'; ?>
             </div>
-            <div class="section-title">ZONA DE CONTROL</div>
+            <div class="section-title zona-control">
+                ZONA DE CONTROL
+            </div>
             <div class="grid-item clas1">
                 <?php include 'components/index/especificaciones.php'; ?>
             </div>
