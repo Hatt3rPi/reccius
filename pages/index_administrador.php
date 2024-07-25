3711d9779b7687481b2fa9aa22232f6f96697fab
lucifxr63 <lucianoalonso2000@gmail.com>
2024-07-25 16:30:38 -0400

    Update index_administrador.php

--- pages/index_administrador.php
+++ pages/index_administrador.php
@@ -30,6 +30,7 @@
                 <?php include 'components/index/cartas.php'; ?>
             </div>
             <div class="grid-item grafo1">
+                <h2>Clima</h2>
                 <?php include 'components/index/laboratorios.php'; ?>
             </div>
             <div class="grid-item grafo2">
