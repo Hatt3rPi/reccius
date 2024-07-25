e281599056abe1553b2e28f5eb60dfd3e4cfd03b
lucifxr63 <lucianoalonso2000@gmail.com>
2024-07-25 17:59:11 -0400

    cartas

--- pages/components/index/cartas.php
+++ pages/components/index/cartas.php
@@ -3,29 +3,56 @@
 <div class="cartas-container">
     <div class="card card-custom card-blue">
         <div class="card-body">
-            <h5 class="card-title" id="prod_liberados" name="prod_liberados">92.31M</h5>
+            <h5 class="card-title" id="prod_liberados">0</h5>
             <p class="card-text">LIBERADOS</p>
         </div>
     </div>
 
     <div class="card card-custom card-green">
         <div class="card-body">
-            <h5 class="card-title" id="prod_enCurso" name="prod_enCurso">79.30M</h5>
-            <p class="card-text">EN CURSO</p>
+            <h5 class="card-title" id="prod_cuarentena">0</h5>
+            <p class="card-text">EN CUARENTENA</p>
         </div>
     </div>
 
     <div class="card card-custom card-orange">
         <div class="card-body">
-            <h5 class="card-title" id="prod_rechazados" name="prod_rechazados">13.02M</h5>
+            <h5 class="card-title" id="prod_rechazados">0</h5>
             <p class="card-text">RECHAZADOS</p>
         </div>
     </div>
 
     <div class="card card-custom card-red">
         <div class="card-body">
-            <h5 class="card-title" id="prod_vencimientos" name="prod_vencimientos">7.20M</h5>
+            <h5 class="card-title" >0</h5>
             <p class="card-text">PRÓXIMOS VENCIMIENTOS<br>(&lt;30 días)</p>
         </div>
     </div>
 </div>
+
+<script>
+    fetch('../pages/backend/components/productos_analisados.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                data.data.forEach(item => {
+                    switch (item.estado.toLowerCase()) {
+                        case 'liberado':
+                            document.getElementById('prod_liberados').innerText = item.contador;
+                            break;
+                        case 'en cuarentena':
+                            document.getElementById('prod_cuarentena').innerText = item.contador;
+                            break;
+                        case 'rechazado':
+                            document.getElementById('prod_rechazados').innerText = item.contador;
+                            break;
+                        default:
+                            console.error('Estado no reconocido:', item.estado);
+                    }
+                });
+            } else {
+                console.error(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+</script>
