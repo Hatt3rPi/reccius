5b45086ef692db8fcaefb301532122f59b0e179e
lucifxr63 <lucianoalonso2000@gmail.com>
2024-07-29 16:22:22 -0400

    fix

--- pages/components/index/acumulados.php
+++ pages/components/index/acumulados.php
@@ -2,13 +2,10 @@
 <link rel="stylesheet" href="../assets/css/components/Index_components/Component_Acumulado.css">
 
 <div class="clas5-container">
-    <div class="card card-custom card-blue">
-        <div class="card-body">
-            <h5 class="card-title" id="clas5_value">0</h5>
-            <p class="card-text">productos analizados acumulados</p>
-        </div>
+    <div class="acumulado">
+        <h5 class="acumulado-value" id="clas5_value">0</h5>
+        <p class="acumulado-text">productos analizados acumulados</p>
     </div>
 </div>
-
 
 <script src="../assets/js/components/acumulados.js"></script>
