96cb28a94b6fe3c6dfc331c3cc9ba6cd0c18d882
Ratapan <javier2000asr@gmail.com>
2024-08-07 22:32:46 -0400

    Replace de url de r2 con '_'

--- pages/backend/cloud/R2_manager.php
+++ pages/backend/cloud/R2_manager.php
@@ -65,11 +65,12 @@ function setFile($params)
   $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
   $contentType = isset($mime_types[$fileExtension]) ? $mime_types[$fileExtension] : 'application/octet-stream';
 
+  $final_path = str_replace( ' ', '_', $folder . '/' . $fileName);
 
   try {
     $result = $R2_client->putObject([
       'Bucket' => $bucket_name,
-      'Key' => $folder . '/' . $fileName,
+      'Key' => $final_path,
       'Body' => $fileBinary,
       'ContentType' => $contentType,
       'ACL' => 'private'
