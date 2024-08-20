2c8266b8433f22f6be122227299adc81c4bae7fc
Hatt3rPi <fabarca212@gmail.com>
2024-08-19 22:43:59 -0400

    Update loginBE.php

--- pages/backend/login/loginBE.php
+++ pages/backend/login/loginBE.php
@@ -5,6 +5,11 @@ ini_set('session.cookie_lifetime', 7200);
 session_start();
 require_once "/home/customw2/conexiones/config_reccius.php";
 
+if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
+    echo json_encode(["exito" => false, "mensaje" => "Sesión no iniciada. Por favor, inicie sesión para continuar.", "idEspecificacion" => 0]);
+    exit(); // Detener la ejecución del script
+}
+
 function escape($value) {
     global $link;
     return mysqli_real_escape_string($link, $value);
