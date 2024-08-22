f9464aeb9f1e5a6aec24e79521d03288ca9a4db0
lucifxr63 <lucianoalonso2000@gmail.com>
2024-08-22 16:01:29 -0400

    Update CALIDAD_documento_actaMuestreo.php

--- pages/CALIDAD_documento_actaMuestreo.php
+++ pages/CALIDAD_documento_actaMuestreo.php
@@ -76,9 +76,8 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                         </tr>
                         <tr>
                             <td>Fecha Muestreo:</td>
-                            <td>
-                                <input type="date" id="fecha_muestreo" name="fecha_muestreo" style="border: 0px;" readonly>
-                            </td>
+                            <td id="fecha_muestreo" name="fecha_muestreo"></td>
+                            
                         </tr>
                     </table>
                 </div>
@@ -789,7 +788,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             var cargo = "<?php echo $_SESSION['cargo']; ?>";
             var fecha_hoy = "<?php echo date('d-m-Y'); ?>";
             var fecha_yoh = "<?php echo date('Y-m-d'); ?>";
-            $('#fecha_muestreo').val(fecha_yoh).prop('readonly', false);
+            $('#fecha_muestreo').text(fecha_yoh).prop('readonly', false);
             $('#fecha_firma1').text(fecha_hoy);
             //document.getElementById('fecha_firma1').style.display = 'block';
             //document.getElementById('mensaje_firma1').style.display = 'block';
@@ -1202,7 +1201,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     $('#form_textarea6').text(response.pregunta6).prop('readonly', false).css('background-color', '#f4fac2');
                     $('#form_textarea7').text(response.pregunta7).prop('readonly', false).css('background-color', '#f4fac2');
                     $('#form_textarea8').text(response.pregunta8).prop('readonly', false).css('background-color', '#f4fac2');
-                    $('#fecha_muestreo').val(fecha_yoh).prop('readonly', false);
+                    $('#fecha_muestreo').text(fecha_yoh).prop('readonly', false);
                     document.getElementById('metodo_muestreo').style.display = 'none';
                     document.getElementById('guardar').style.display = 'block';
                     $('.resp').css('background-color', '#f4fac2');
@@ -1216,7 +1215,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     $('#form_textarea6').text(response.pregunta6).prop('readonly', true);
                     $('#form_textarea7').text(response.pregunta7).prop('readonly', true);
                     $('#form_textarea8').text(response.pregunta8).prop('readonly', true);
-                    $('#fecha_muestreo').val(response.fecha_muestreo).prop('readonly', true);
+                    $('#fecha_muestreo').text(response.fecha_muestreo).prop('readonly', true);
                     firma1(response);
                     $('#etapa').text('ingresa resultados y firma2');
                     if (usuario_activo == response.responsable) {
@@ -1232,7 +1231,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     $('#form_textarea6').text(response.pregunta6).prop('readonly', true);
                     $('#form_textarea7').text(response.pregunta7).prop('readonly', true);
                     $('#form_textarea8').text(response.pregunta8).prop('readonly', true);
-                    $('#fecha_muestreo').val(response.fecha_muestreo).prop('readonly', true);
+                    $('#fecha_muestreo').text(response.fecha_muestreo).prop('readonly', true);
                     firma1(response);
                     firma2(response);
                     $('#etapa').text('firma3');
@@ -1246,7 +1245,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     $('#form_textarea6').text(response.pregunta6).prop('readonly', true);
                     $('#form_textarea7').text(response.pregunta7).prop('readonly', true);
                     $('#form_textarea8').text(response.pregunta8).prop('readonly', true);
-                    $('#fecha_muestreo').val(response.fecha_muestreo).prop('readonly', true);
+                    $('#fecha_muestreo').text(response.fecha_muestreo).prop('readonly', true);
                     firma1(response);
                     firma2(response);
                     firma3(response);
