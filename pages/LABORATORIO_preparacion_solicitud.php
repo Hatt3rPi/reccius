3c607fc5c20baaee54ddc6866f6a5828482480aa
Hatt3rPi <fabarca212@gmail.com>
2024-08-26 14:13:52 -0400

    Update LABORATORIO_preparacion_solicitud.php

--- pages/LABORATORIO_preparacion_solicitud.php
+++ pages/LABORATORIO_preparacion_solicitud.php
@@ -128,7 +128,7 @@ while ($row = mysqli_fetch_assoc($result)) {
                     <div class="divider"></div>
                     <div class="form-group">
                         <label>Condiciones Almacenamiento:</label>
-                        <textarea required class="form-control mx-0 w-90 border rounded-sm " style="field-sizing: content;" name="condicion_almacenamiento" id="condicion_almacenamiento" rows="2" placeholder="..."></textarea>
+                        <textarea required class="form-control mx-0 w-90 border rounded-sm " style="field-sizing: content;" name="condicion_almacenamiento" id="condicion_almacenamiento" rows="2" value="T° ambiente, lugar fresco y seco, protegido de la luz"></textarea>
                     </div>
                 </div>
                 <div class="form-row">
@@ -150,7 +150,7 @@ while ($row = mysqli_fetch_assoc($result)) {
                     <div class="divider"></div> <!-- Esta es la línea divisora -->
                     <div class="form-group">
                         <label>Muestreado según POS:</label>
-                        <input required class="form-control mx-0 w-90 " name="numero_pos" id="numero_pos" type="text" placeholder="...">
+                        <input required class="form-control mx-0 w-90 " name="numero_pos" id="numero_pos" type="text" value="DCAL-CC-PO-007">
                     </div>
                 </div>
                 <div class="form-row">
