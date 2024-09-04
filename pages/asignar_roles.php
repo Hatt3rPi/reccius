814d7b6b554319a8b82f3433955c77e886e91484
lucifxr63 <lucianoalonso2000@gmail.com>
2024-09-04 17:38:09 -0400

    Update asignar_roles.php

--- pages/asignar_roles.php
+++ pages/asignar_roles.php
@@ -4,8 +4,6 @@
 <head>
     <link rel="stylesheet" href="../assets/css/Listados.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 </head>
 
 <body>
