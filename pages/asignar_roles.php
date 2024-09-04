24ce0f06044c995eada3e59f829dc59385e22743
lucifxr63 <lucianoalonso2000@gmail.com>
2024-09-04 17:21:58 -0400

    Update asignar_roles.php

--- pages/asignar_roles.php
+++ pages/asignar_roles.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Usuarios</title>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="../assets/css/Listados.css">
 </head>
 <body>
     <div class="container">
