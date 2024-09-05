af5dd217608aa5aabcc0a11470891854faa6469a
Hatt3rPi <fabarca212@gmail.com>
2024-09-04 22:45:27 -0400

    Update listado_productosDisponiblesBE.php

--- pages/backend/acta_liberacion/listado_productosDisponiblesBE.php
+++ pages/backend/acta_liberacion/listado_productosDisponiblesBE.php
@@ -19,7 +19,7 @@ $query = "SELECT
             FROM calidad_productos_analizados as a
             LEFT JOIN calidad_productos as b on a.id_producto=b.id
             left join calidad_analisis_externo as c on a.id_analisisExterno=c.id
-            left join calidad_acta_muestreo as d on a.id_analisisExterno=d.id_analisisExterno
+            left join calidad_acta_muestreo as d on a.id_actaMuestreo=d.id
             where a.estado not in ('eliminado_por_solicitud_usuario');";
 $result = $link->query($query);
 
