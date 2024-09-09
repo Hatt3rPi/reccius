092a15943b82bfa5d05ff591ca48099a43c7e00d
Hatt3rPi <fabarca212@gmail.com>
2024-09-08 23:08:35 -0300

    Update CALIDAD_documento_analisisExterno.php

--- pages/CALIDAD_documento_analisisExterno.php
+++ pages/CALIDAD_documento_analisisExterno.php
@@ -80,7 +80,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                         <td class="titulo">1. Laboratorio Analista:(*)</td>
                         <td><input type="text" id="laboratorio" name="laboratorio" required></td>
                         <td class="titulo titulo-right">Tipo de Analisis:</td>
-                        <td><input type="text" id="Tipo_Analisis" name="Tipo_Analisis" required></td>
+                        <td><input type="text" id="tipo_analisis" name="tipo_analisis" required></td>
 
                     </tr>
                     <tr>
