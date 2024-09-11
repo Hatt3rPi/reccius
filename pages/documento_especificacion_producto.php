97b8e79e077b4e4132003bfc22890aa1378d5c52
lucifxr63 <lucianoalonso2000@gmail.com>
2024-09-11 15:49:55 -0300

    mega try

--- pages/documento_especificacion_producto.php
+++ pages/documento_especificacion_producto.php
@@ -310,16 +310,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             });
         });
 
-
-
-
-
-
-
-
-
-
-
+        // Función para cargar los datos de la especificación
         function cargarDatosEspecificacion(id) {
             console.log("A1")
             $.ajax({
@@ -338,6 +329,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             });
         }
 
+        // Procesar los datos de la especificación
         async function procesarDatosEspecificacion(response) {
             console.log("A2")
             // Validación de la respuesta
@@ -372,6 +364,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             });
         }
 
+        // Función para mostrar la imagen de firma
         function mostrarImagenFirma(usuario, contenedorQR) {
             console.log("A4")
             var firmaUrl;
@@ -406,10 +399,11 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     });
             } else {
                 var contenedor = document.getElementById(contenedorQR);
-                contenedor.innerHTML = '<span  src="https://pub-bde9ff3e851b4092bfe7076570692078.r2.dev/firma_no_proporcionada.webp" style="display: inline-block; width: 64px; height: 64px; line-height: 64px; text-align: center;"></span>';
+                contenedor.innerHTML = '<span src="https://pub-bde9ff3e851b4092bfe7076570692078.r2.dev/firma_no_proporcionada.webp" style="display: inline-block; width: 64px; height: 64px; line-height: 64px; text-align: center;"></span>';
             }
         }
 
+        // Función para poblar los datos de los campos
         function poblarYDeshabilitarCamposProducto(producto) {
             console.log("A3")
             if (producto) {
@@ -466,6 +460,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             }
         }
 
+        // Función para mostrar el análisis FQ
         function mostrarAnalisisFQ(analisis) {
             console.log("A5")
             // Verifica si hay datos para el análisis FQ
@@ -519,6 +514,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             }
         }
 
+        // Función para mostrar el análisis MB
         function mostrarAnalisisMB(analisis) {
             console.log("A6")
             // Verifica si hay datos para el análisis microbiológico
@@ -592,7 +588,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
 
         function firmarDocumento() {
             console.log("A9")
-            // Obten los datos necesarios para la firmaid_especificacion
+            // Obten los datos necesarios para la firma
             var idEspecificacion = $('#id_especificacion').text().trim();
             var rolUsuario = '';
             if (esRevisorYFirmaPendiente()) {
@@ -600,7 +596,6 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             } else if (esAprobadorYFirmaPendiente()) {
                 rolUsuario = 'aprobado_por';
             } else {
-                // En caso de que no se pueda determinar el rol, mostrar un error o tomar una acción adecuada
                 console.error("No se puede determinar el rol del usuario para la firma.");
                 return;
             }
@@ -614,12 +609,9 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                     rolUsuario: rolUsuario
                 },
                 success: function(response) {
-                    // Aquí manejas la respuesta del backend
                     console.log('Firma actualizada correctamente:', response);
-                    // Actualiza el estado del documento en el frontend
                     actualizarEstadoDocumento();
                     cargarDatosEspecificacion(idEspecificacion);
-                    // Mostrar notificaciones de éxito y advertencia
                     $.notify("Documento firmado con éxito.", "success");
                     $.notify("Tarea terminada con éxito", "success");
                 },
@@ -655,23 +647,19 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             let esAprobadorPendiente = esAprobador && !fecha_aprobacion;
             let revisorHaFirmado = !!fecha_revision;
 
-            // Ocultar botón si no corresponde al usuario
             let botonFirma = document.getElementById('sign-document');
             if (!botonFirma) return;
 
-            // Si es el revisor y la firma del aprobador está pendiente, ocultar botón
             if (esRevisor && fecha_aprobacion !== null) {
                 botonFirma.style.display = 'none';
                 return;
             }
 
-            // Si es el aprobador y la firma del revisor está pendiente, ocultar botón
             if (esAprobador && fecha_revision === null) {
                 botonFirma.style.display = 'none';
                 return;
             }
 
-            // Mostrar botón según las condiciones establecidas
             if (esRevisorPendiente) {
                 console.log("Mostrar botón de firma para Revisor (habilitado)");
                 botonFirma.style.display = 'block';
@@ -687,7 +675,6 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 botonFirma.title = "Documento debe estar firmado por revisor para poder aprobarlo";
             }
         }
-
 
         function actualizarEstadoDocumento() {
             console.log("A11")
@@ -742,6 +729,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
             }
         }
     </script>
+
 
 </body>
 
