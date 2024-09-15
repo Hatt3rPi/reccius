48163ae331e2a02aa84b446d5d5a4c55854c29b8
Hatt3rPi <fabarca212@gmail.com>
2024-09-15 12:13:17 -0300

    incorporación versiones Acta Muestreo

---
+++ pages/backend/acta_muestreo/rechazar_acta_muestreoBE.php
@@ -0,0 +1,62 @@
+<?php
+// archivo: pages\backend\analisis\eliminar_analisis_externoBE.php
+session_start();
+require_once "/home/customw2/conexiones/config_reccius.php";
+
+// Comprobar si el ID fue enviado por POST
+if (!isset($_POST['id'])) {
+    echo json_encode(['error' => 'No se proporcionó el ID necesario.']);
+    exit;
+}
+
+// Verificar la conexión
+if ($link->connect_error) {
+    die("Conexión fallida: " . $link->connect_error);
+}
+
+// Inicia la transacción
+$link->begin_transaction();
+$id_actaMuestreo = intval($_POST['id']);
+$motivo_rechazo = $_POST['motivo_rechazo'];
+$fecha_rechazo = $_POST['fecha_rechazo'];
+
+$resultados = [];
+
+try {
+    // 1. Actualiza en calidad_analisis_externo si existe
+    $stmt = $link->prepare("UPDATE calidad_acta_muestreo SET estado = 'rechazado', motivo_rechazo =?, fecha_rechazo =? WHERE id = ?");
+    if ($stmt) {
+        $stmt->bind_param("ssi", $motivo_rechazo, $fecha_rechazo , $id_actaMuestreo);
+        $stmt->execute();
+        $resultados[] = "calidad_acta_muestreo: actualización exitosa";
+        $stmt->close();
+
+        registrarTrazabilidad(
+            $_SESSION['usuario'],
+            $_SERVER['PHP_SELF'],
+            'Eliminación AEX',
+            'CALIDAD - Solicitud Análisis Externo',
+            0, // ID del registro insertado
+            "UPDATE calidad_acta_muestreo SET estado = 'rechazado', motivo_rechazo =?, fecha_rechazo =? WHERE id = ?",
+            [$motivo_rechazo, $fecha_rechazo , $id_actaMuestreo],
+            1,
+            null
+        );
+    } else {
+        throw new Exception("Error al preparar la declaración para calidad_acta_muestreo: " . $link->error);
+    }
+
+    // Confirma la transacción
+    $link->commit();
+    echo json_encode(['resultados' => $resultados], JSON_UNESCAPED_UNICODE);
+
+} catch (Exception $e) {
+    // Si algo falla, realiza un rollback
+    $link->rollback();
+    $resultados[] = "Error: " . $e->getMessage();
+    echo json_encode(['resultados' => $resultados]);
+} finally {
+    // Cerrar la conexión
+    $link->close();
+}
+?>
