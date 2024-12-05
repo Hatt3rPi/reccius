2b7705b39ec915375c5e2c2fd975deb2093463b6
Lucii.fxr <63132793+lucifxr63@users.noreply.github.com>
2024-12-05 19:32:55 -0300

    produccion

--- pages/Produccion/Produccion.php
+++ pages/Produccion/Produccion.php
@@ -16,7 +16,7 @@
             <div class="column">
                 <div class="form-group">
                     <label for="fecha-revision">Fecha Revisión:</label>
-                    <input type="text" id="fecha-revision" name="fecha-revision" value="(automática)" disabled>
+                    <input type="text" id="fecha-revision" name="fecha-revision" value="(automática)" class="input-disabled" disabled>
                 </div>
                 <div class="form-group">
                     <label for="medio-despacho">Medio Despacho:</label>
@@ -24,29 +24,29 @@
                 </div>
                 <div class="form-group">
                     <label for="dia-retiro">Día Retiro:</label>
-                    <input type="text" id="dia-retiro" name="dia-retiro" value="(automática según regla)" disabled>
+                    <input type="text" id="dia-retiro" name="dia-retiro" value="(automática según regla)" class="input-disabled" disabled>
                 </div>
                 <div class="form-group">
                     <label for="hora-retiro">Hora Retiro:</label>
-                    <input type="text" id="hora-retiro" name="hora-retiro" value="(automática según regla)" disabled>
+                    <input type="text" id="hora-retiro" name="hora-retiro" value="(automática según regla)" class="input-disabled" disabled>
                 </div>
             </div>
             <div class="column">
                 <div class="form-group">
                     <label for="mismo-retiro">¿Mismo retiro para todos los productos?:</label>
-                    <input type="text" id="mismo-retiro" name="mismo-retiro" value="Sí/No" disabled>
+                    <input type="text" id="mismo-retiro" name="mismo-retiro" value="Sí/No" class="input-disabled" disabled>
                 </div>
                 <div class="form-group">
                     <label for="numero-producto"># Producto:</label>
-                    <input type="text" id="numero-producto" name="numero-producto" value="1 (automática)" disabled>
+                    <input type="text" id="numero-producto" name="numero-producto" value="1 (automática)" class="input-disabled" disabled>
                 </div>
                 <div class="form-group">
                     <label for="producto">Producto:</label>
-                    <input type="text" id="producto" name="producto" value="(cargado desde BE)" disabled>
+                    <input type="text" id="producto" name="producto" value="(cargado desde BE)" class="input-disabled" disabled>
                 </div>
                 <div class="form-group">
                     <label for="area-fabricacion">Área Fabricación:</label>
-                    <input type="text" id="area-fabricacion" name="area-fabricacion" value="(cargado desde BE)" disabled>
+                    <input type="text" id="area-fabricacion" name="area-fabricacion" value="(cargado desde BE)" class="input-disabled" disabled>
                 </div>
             </div>
         </div>
@@ -55,17 +55,7 @@
         <div id="comments-container">
             <h2>Ingresar Comentario</h2>
             <div class="form-group">
-                <input type="text" id="comment" name="comment" placeholder="Escribe tu comentario aquí">
-            </div>
-        </div>
-
-        <!-- Estado -->
-        <div id="status-container">
-            <h2>Estado:</h2>
-            <div class="form-group">
-                <span style="color: white; background-color: red; padding: 10px; border-radius: 4px;">
-                    3. Listo para facturar
-                </span>
+                <textarea id="comment" name="comment" placeholder="Escribe tu comentario aquí" rows="3"></textarea>
             </div>
         </div>
     </div>
