e0417d939b848c9af7f680bb80933e87cfd66604
Hatt3rPi <fabarca212@gmail.com>
2025-02-06 21:43:13 -0300

    Update LABORATORIO_listado_solicitudes.php

--- pages/LABORATORIO_listado_solicitudes.php
+++ pages/LABORATORIO_listado_solicitudes.php
@@ -265,7 +265,7 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
                 
             }
             if (d.estado === "Pendiente envío a Laboratorio" ) {
-                acciones += `<button class="accion-btn ingControl" title="Revisar Documento" id="${d.id_analisisExterno}" name="generar_documento_pdf" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i> Revisar solicitud</button><a> </a>`;
+                //acciones += `<button class="accion-btn ingControl" title="Revisar Documento" id="${d.id_analisisExterno}" name="generar_documento_pdf" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i> Revisar solicitud</button><a> </a>`;
                 if (d.enviado_lab_por === usuarioActual || usuarioActual === 'isumonte'){
                     acciones += `<button class="accion-btn ingControl" title="Enviar a Laboratorio" id="${d.id_analisisExterno}" name="enviarSolicitud_laboratorio" onclick="botones(this.id, this.name, \'laboratorio\')"><i class="fa fa-file-pdf-o"></i> Enviar Solicitud a Laboratorio</button><a> </a>`;
                 }
