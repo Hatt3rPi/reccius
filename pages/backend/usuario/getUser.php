7791a28760467dbdaeb6af73a0a60772589a08d0
Ratapan <javier2000asr@gmail.com>
2025-02-17 14:13:30 -0300

    Modificar getUser.php para devolver usuarios en formato JSON

--- pages/backend/usuario/getUser.php
+++ pages/backend/usuario/getUser.php
@@ -6,11 +6,12 @@ if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
     exit;
 }
 
+header('Content-Type: application/json; charset=utf-8');
 
-$query = "SELECT * FROM usuario WHERE nombre LIKE '%" . mysqli_real_escape_string($this->link, $_GET['nombre']) . "%'";
-$result = mysqli_query($this->link, $query);
+$query = "SELECT * FROM usuario WHERE nombre LIKE '%" . mysqli_real_escape_string($link, $_GET['nombre']) . "%'";
+$result = mysqli_query($link, $query);
 $users = [];
 while ($row = mysqli_fetch_assoc($result)) {
     $users[] = $row;
 }
-return $users;
+echo json_encode($users);
